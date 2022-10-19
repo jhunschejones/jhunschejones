098b524b556e2a70bc8d1c531f970d2699117349
Joshua Hunsche Jones <jhunschejones@github.com>
2022-10-19 10:55:46 -0500

    Only show kana if it's different from the word

--- wotd.rb
+++ wotd.rb
@@ -9,8 +9,7 @@ readme = File.read("README.md")
 wotd = <<-WOTD
 <!-- START WORD OF THE DAY -->
 <table>
-  <tr><td><strong>Japanese:</strong> <a href="#{scraper.word_audio_url}">🔊</a></td><td>#{scraper.word}</td></tr>
-  <tr><td><strong>Kana:</strong></td><td>#{scraper.kana}</td></tr>
+  <tr><td><strong>Japanese:</strong> <a href="#{scraper.word_audio_url}">🔊</a></td><td>#{scraper.word}</td></tr>#{scraper.word != scraper.kana ? "\n  <tr><td><strong>Kana:</strong></td><td>#{scraper.kana}</td></tr>" : ""}
   <tr><td><strong>Part of speech:</strong></td><td>#{scraper.part_of_speach}</td></tr>
   <tr><td><strong>English:</strong></td><td>#{scraper.translation}</td></tr>
   <tr><td><strong>Example sentence:</strong> <a href="#{scraper.sentence_audio_url}">🔊</a></td><td>#{scraper.example_sentence}</td></tr>
